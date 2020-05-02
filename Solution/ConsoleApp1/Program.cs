2d7115b7d08fc94c5ee6e41ec1d00a8e5e617067
houssemshili <48476266+houssemshili@users.noreply.github.com>
2020-05-02 19:34:07 +0200

    first commit

---
+++ Solution/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"C:\Temp\HES.txt";
+            //modify this comment
+            //modify this comment
+            try
+            {
+                // Check if file already exists. If yes, delete it.     
+                if (File.Exists(fileName))
+                {
+                    File.Delete(fileName);
+                }
+
+                // Create a new file     
+                using (FileStream fs = File.Create(fileName))
+                {
+                    // Add some text to file    
+                    Byte[] title = new UTF8Encoding(true).GetBytes("New Text File");
+                    fs.Write(title, 0, title.Length);
+                    byte[] author = new UTF8Encoding(true).GetBytes("Houssem Shili");
+                    fs.Write(author, 0, author.Length);
+                }              
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex.ToString());
+            }
+        }
+    }    
+}
---
+++ Solution/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 1, b = 1;
+            Assert.IsTrue(a == b);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int a = 1, b = 2;
+            Assert.IsTrue(a == b);
+        }
+    }
+}
