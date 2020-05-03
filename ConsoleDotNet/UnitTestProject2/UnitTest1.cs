2a2412cebcc09d7e0c987245fd31c54b1a49bb0f
houssemshili <48476266+houssemshili@users.noreply.github.com>
2020-05-03 12:41:22 +0200

    commit new solution

---
+++ ConsoleDotNet/UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 1, b = 1;
+            Assert.IsTrue(a == b);
+        }
+    }
+}
